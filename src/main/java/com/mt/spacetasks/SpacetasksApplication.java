26f4163fa32cd3be86fc85cb7f9b77656e8575eb
netshiaMR <netshiarendani@gmail.com>
2020-01-28 15:49:27 +0200

    First Commit for mtspaceTasks

---
+++ src/main/java/com/mt/spacetasks/SpacetasksApplication.java
@@ -0,0 +1,13 @@
+package com.mt.spacetasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacetasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacetasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mt/spacetasks/SpacetasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.spacetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacetasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
